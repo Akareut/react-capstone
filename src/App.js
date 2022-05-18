d96c12d732437dd14b87757431290aeacb3ea19b
TAATA EARL <fredvuni809@gmail.com>
2022-05-18 23:35:48 +0300

    Added the blueprint or structure for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <NavBar/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>  
+              <Route path="/cart" element={<Cart/>}/>  
+              <Route path="/sign-in" element={<SignIn/>}/>  
+              <Route path="/sign-up" element={<SignUp/>}/>  
+          </Routes>
+          <Footer/>
+      </Router>
     </div>
   );
 }
