1c2dec1cf6c1b8e405766d9077f6be3fbd52630b
TAATA EARL <fredvuni809@gmail.com>
2022-05-19 12:45:46 +0300

    Added a footer and header component

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+// import SliderData from '../SliderData'
 
 const BannerSection = () =>{
     return (
-        <div>
-            <h1>Banner</h1>
-        </div>
+        <>
+            {/* {
+                SliderData.map((slider,index)=>{
+                    return(
+                        <div className="box d_flex top" key={index}>
+                            <div className="left">
+                                <h1>{slider.title}</h1>
+                                <p>{slider.desc}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            } */}
+        </>
     )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../App.css'
 
 const NavBar = () => {
+
+    function toggleMenu(){
+        let menuItems =document.querySelector("#menuItems")
+        menuItems.style.maxHeight = "0px"
+
+        if(menuItems.style.maxHeight === "0px"){
+            menuItems.style.maxHeight = "200px"
+        }else{
+            menuItems.style.maxHeight = "0px"
+            }
+        }
+
     return (
-        <div>
-            <h1>Nav bar</h1>
-            <Link to="/">Home</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/sign-in">Sign in</Link>
-            <Link to="/sign-up">Sign up</Link>
+        <div className="container">
+        <div className="navbar">
+            <div className="logo">
+                <Link to="/">
+                    <h4>Iconic<span>Online store...</span></h4>
+                </Link>
+            </div>
+            <nav>
+                <ul id="menuItems">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li className="dropdown">
+                        <Link to=""> Account
+                            <i className="fa fa-chevron-down"></i>
+                        </Link>
+                        <ul className="dropdown-content">
+                            <Link to="/sign-in" id="log">Login</Link>
+                            <Link to="/sign-up" id="reg">Register</Link>
+                        </ul>
+                    </li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+            <Link to="/cart"><i className="fas fa-shopping-cart cart"> <span id="itemsNum"></span></i></Link>
+            <i className="fas fa-bars menu" onClick={toggleMenu}></i>
         </div>
+    </div>
     )
 }
 
